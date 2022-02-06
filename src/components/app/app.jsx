66233e2dfb7ee18c0693051199ac629a0c54e486
Peter-PM <tangompp@gmail.com>
2022-02-07 02:49:35 +0300

    popup bugfix

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,15 +3,20 @@ import Header from '../header/header';
 import Footer from '../footer/footer';
 import Main from '../main/main';
 import Popup from '../popup/popup';
+import { useSelector } from 'react-redux';
+import { getPopupView } from '../../store/selectors';
 import './app.module.scss';
 
 function App() {
+
+  const popupView = useSelector(getPopupView);
+  
   return (
     <>
       <Header/>
       <Main/>
       <Footer/>
-      <Popup/>
+      {popupView ? <Popup/> : ''}
     </>
   );
 }
--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -7,7 +7,6 @@ import { getPopupCard, getPopupView } from "../../store/selectors";
 const FocusTrap = require("focus-trap-react");
 
 function Popup() {
-
   const [activeCard, setCard] = useState(useSelector(getPopupCard));
 
   const popupView = useSelector(getPopupView);
@@ -19,11 +18,15 @@ function Popup() {
   }, [popupCard]);
 
   useEffect(() => {
-    popupView ? document.addEventListener("keydown", handleEscKeyDown) : document.removeEventListener("keydown", handleEscKeyDown);
+    popupView
+      ? document.addEventListener("keydown", handleEscKeyDown)
+      : document.removeEventListener("keydown", handleEscKeyDown);
   }, [popupView]);
 
-  useEffect(()=> {
-    document.querySelector('body').style.overflow = popupView ? "hidden" : "visible" ;
+  useEffect(() => {
+    document.querySelector("body").style.overflow = popupView
+      ? "hidden"
+      : "visible";
   }, [popupView]);
 
   const handleClosePopup = () => {
@@ -62,112 +65,104 @@ function Popup() {
 
   return (
     <>
-      {popupView ? (
-        <FocusTrap>
+      <FocusTrap>
+        <section className={styles.container} onClick={handleClosePopup}>
           <section
-            className={styles.container}
-            onClick={handleClosePopup}
+            className={styles.popup}
+            onClick={(evt) => evt.stopPropagation()}
           >
-            <section
-              className={styles.popup}
-              onClick={(evt) => evt.stopPropagation()}
+            <h2 className={styles.title}>Редактировать карточку</h2>
+            <button
+              className={styles.close}
+              type="button"
+              onClick={handleClosePopup}
             >
-              <h2 className={styles.title}>Редактировать карточку</h2>
-              <button
-                className={styles.close}
-                type="button"
-                onClick={handleClosePopup}
+              <svg
+                width="15"
+                height="16"
+                viewBox="0 0 15 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <svg
-                  width="15"
-                  height="16"
-                  viewBox="0 0 15 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M13.6399 15.0096L7.50482 8.86495L1.36977 15.0096L0 13.6399L6.14469 7.50482L0 1.36978L1.36977 0L7.50482 6.14469L13.6399 0.00964652L15 1.36978L8.86495 7.50482L15 13.6399L13.6399 15.0096Z"
-                    fill="#9F9E9E"
-                  />
-                </svg>
+                <path
+                  d="M13.6399 15.0096L7.50482 8.86495L1.36977 15.0096L0 13.6399L6.14469 7.50482L0 1.36978L1.36977 0L7.50482 6.14469L13.6399 0.00964652L15 1.36978L8.86495 7.50482L15 13.6399L13.6399 15.0096Z"
+                  fill="#9F9E9E"
+                />
+              </svg>
+            </button>
+            <form className={styles.form} name="form">
+              <input
+                className={styles.input}
+                type="text"
+                name="title"
+                placeholder="Введите заголовок"
+                autoFocus
+                value={activeCard.title}
+                onChange={(evt) => {
+                  setCard({
+                    ...activeCard,
+                    title: evt.target.value,
+                  });
+                }}
+                onKeyPress={handleEnterKeyDown}
+              />
+
+              <input
+                className={styles.input}
+                type="text"
+                name="title"
+                placeholder="Введите текст"
+                value={activeCard.text}
+                onChange={(evt) => {
+                  setCard({
+                    ...activeCard,
+                    text: evt.target.value,
+                  });
+                }}
+                onKeyPress={handleEnterKeyDown}
+              />
+
+              <input
+                className={styles.input}
+                type="number"
+                name="width"
+                min="1"
+                max="12"
+                value={activeCard.width}
+                onChange={(evt) => {
+                  setCard({
+                    ...activeCard,
+                    width: +evt.target.value,
+                  });
+                }}
+                onKeyPress={handleEnterKeyDown}
+              />
+
+              <button
+                className={styles.button}
+                type="button"
+                onClick={handleSaveCanges}
+              >
+                Сохранить изменения
               </button>
-              <form className={styles.form} name="form">
-                <input
-                  className={styles.input}
-                  type="text"
-                  name="title"
-                  placeholder="Введите заголовок"
-                  autoFocus
-                  value={activeCard.title}
-                  onChange={(evt) => {
-                    setCard({
-                      ...activeCard,
-                      title: evt.target.value,
-                    });
-                  }}
-                  onKeyPress={handleEnterKeyDown}
-                />
-
-                <input
-                  className={styles.input}
-                  type="text"
-                  name="title"
-                  placeholder="Введите текст"
-                  value={activeCard.text}
-                  onChange={(evt) => {
-                    setCard({
-                      ...activeCard,
-                      text: evt.target.value,
-                    });
-                  }}
-                  onKeyPress={handleEnterKeyDown}
-                />
-
-                <input
-                  className={styles.input}
-                  type="number"
-                  name="width"
-                  min="1"
-                  max="12"
-                  placeholder="Ширина от 1 до 12"
-                  value={activeCard.width}
-                  onChange={(evt) => {
-                    setCard({
-                      ...activeCard,
-                      width: +evt.target.value,
-                    });
-                  }}
-                  onKeyPress={handleEnterKeyDown}
-                />
-
-                <button
-                  className={styles.button}
-                  type="button"
-                  onClick={handleSaveCanges}
-                >
-                  Сохранить изменения
-                </button>
-                <button
-                  className={`${styles.button} ${styles.button_add}`}
-                  type="button"
-                  onClick={handleAddCanges}
-                >
-                  Добавить карточку
-                </button>
-                <button
-                  className={`${styles.button} ${styles.button_delete}`}
-                  type="button"
-                  onClick={handleDelButton}
-                >
-                  Удалить карточку
-                </button>
-              </form>
-            </section>
+              <button
+                className={`${styles.button} ${styles.button_add}`}
+                type="button"
+                onClick={handleAddCanges}
+              >
+                Добавить карточку
+              </button>
+              <button
+                className={`${styles.button} ${styles.button_delete}`}
+                type="button"
+                onClick={handleDelButton}
+              >
+                Удалить карточку
+              </button>
+            </form>
           </section>
-        </FocusTrap>
-      ) : (
-        false
-      )}
+        </section>
+      </FocusTrap>
     </>
   );
 }
